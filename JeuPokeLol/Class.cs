5b2c697cd2c0d932fef2358783191e1db88d2779
Enzo-Naox <erosalie@gaming.tech>
2024-02-06 21:39:24 +0100

    6fev 21h

---
+++ JeuPokeLol/Class.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Statistiques
+{
+    public float Vie { get; set; }
+    public float Attaque { get; set; }
+    public float Defense { get; set; }
+    public float Vitesse { get; set; }
+}
+
+
+public class Pokelol
+{
+    public string Name { get; set; }
+    public int Level { get; set; }
+    public float Experience_Acquis { get; set; }
+    public float Experience_Necessaire { get; set; }
+    public Statistiques Stats { get; set; }
+    public List<Capacite> Capacites { get; set; }
+
+    public Pokelol(string name)
+    {
+        Name = name;
+
+        Level = 0;
+        Experience_Acquis = 0;
+        Experience_Necessaire = 100;
+        Stats = new Statistiques();
+
+        Capacites = new List<Capacite> { };
+    }
+
+    public void DefinirStatistiques(float vie, float attaque, float defense, float vitesse)
+    {
+        Stats.Vie = vie;
+        Stats.Attaque = attaque;
+        Stats.Defense = defense;
+        Stats.Vitesse = vitesse;
+    }
+
+    public void MonterDeNiveau()
+    {
+        if (Experience_Acquis >= Experience_Necessaire)
+        {
+            Level++;
+            Experience_Necessaire *= 1.05f;
+            Stats.Attaque += 5;
+            Stats.Defense += 3;
+            Stats.Vie += 15;
+        }
+    }
+
+
+    public void AddCapacite(Capacite capacite)
+    {
+        Capacites.Add(capacite);
+    }
+
+
+
+    public class Capacite
+    {
+        public string Name_capacite { get; set; }
+        public string Description { get; set; }
+
+        public float Stats_capacite { get; set; }
+        public string Type_capacite { get; set; }
+
+        public Capacite(string name_capacite, string description, float stats_capacite, string type_capacite)
+        {
+            Name_capacite = name_capacite;
+            Description = description;
+            Stats_capacite = stats_capacite;
+            Type_capacite = type_capacite;
+        }
+    }
+}
+
+
+
+public class Player
+{
+    public string Name { get; set; }
+
+    public int Level { get; set; }
+    public float Experience_Acquis { get; set; }
+    public float Experience_Necessaire { get; set; }
+
+    public Inventory Inventory { get; set; }
+    public Team Team { get; set; }
+
+
+    public Player(string name)
+    {
+        Name = name;
+
+        Level = 1;
+        Experience_Acquis = 0;
+        Experience_Necessaire = 100;
+
+        Inventory = new Inventory();
+        Team = new Team();
+    }
+
+
+}
+
+public class Inventory
+{
+    public List<Item> Items { get; set; } = new List<Item>();
+
+    public void AddItem(Item item)
+    {
+        Items.Add(item);
+    }
+
+    public void RemoveItem(Item item)
+    {
+        Items.Remove(item);
+    }
+
+    public void ShowInventory()
+    {
+        Console.WriteLine("Inventory Contents:");
+        foreach (var item in Items)
+        {
+            Console.WriteLine($"Name: {item.Name}");
+        }
+    }
+
+}
+    
+public class Item
+{
+    public string Name { get; set; }
+}
+
+public class Team
+{
+    public List<Pokelol> Members { get; set; } = new List<Pokelol>();
+
+    public void AddMember(Pokelol Name)
+    {
+        Members.Add(Name);
+    }
+
+    public void ShowTeam()
+    {
+        Console.WriteLine("Team Members:");
+        foreach (var member in Members)
+        {
+            Console.WriteLine($"Name: {member.Name}, HP: {member.Stats.Vie}, Attack: {member.Stats.Attaque}, Defense: {member.Stats.Defense}, Speed: {member.Stats.Vitesse}");
+        }
+    }
+}
--- JeuPokeLol/Program.cs
+++ JeuPokeLol/Program.cs
@@ -1,72 +1,23 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Net.NetworkInformation;
+using System.Numerics;
 using System.Reflection.Metadata;
-
-
-
-Pokelol Joueur = new("Pikachu", 1, 1, 1, 1);
-Pokelol Ennemi = new("Pikachu", 1, 1, 1, 1);
-
-
-Pokelol Pikaren = new("Pikaren", 233, 183, 147, 151);
-Pokelol Ectoplari = new("Ectoplari", 213, 162, 142, 182);
-Pokelol Swainquaza = new("Swainquaza", 212, 202, 142, 147);
-Pokelol Ahram = new("Ahram", 207, 137, 152, 187);
-
-
-//Creation de Capacités
-
-//Pikaren
-Pokelol.Capacite Pikaren_capacite1 = new("Elekdemacia = ", " Inflige des degats (80 puissance)",80, "Degat");
-Pokelol.Capacite Pikaren_capacite2 = new("Pikarush = ", "Augmente la vitesse de 15%", 1.15f, "Speed+");
-Pokelol.Capacite Pikaren_capacite3 = new("Gabri = ", "Augmente la defense de 30%", 1.3f, "Defense+");
-//Ectoplari
-Pokelol.Capacite Ectoplari_capacite1 = new("LaserOmbre = " , "Inflige des degats(110 puissance)", 110, "Degat");
-Pokelol.Capacite Ectoplari_capacite2 = new("Exploplasma = " , "Inflige des degats(90 puissance)", 90, "Degat");
-Pokelol.Capacite Ectoplari_capacite3 = new("Taclebas = ", "Baisse la vitesse de l'ennemi de 20% ", 0.8f, "Speed-");
-//Swainquaza
-Pokelol.Capacite Swainquaza_capacite1 = new("CorbeauMeteore = ", "Inflige des degats(150 puissance)", 150, "Degat");
-Pokelol.Capacite Swainquaza_capacite2 = new("GriffeAscension = ", "Inflige des degats(110 puissance)", 110, "Degat");
-Pokelol.Capacite Swainquaza_capacite3 = new("OeilViolent = ", "Inflige des degats(90 puissance)", 90, "Degat");
-//Ahram
-Pokelol.Capacite Ahram_capacite1 = new("FlammeCharme = ", " Baisse l'attaque de l'adversaire de 30% ", 0.7f, "Attaque-");
-Pokelol.Capacite Ahram_capacite2 = new("Dracoboule = ", "Inflige des degats(85 puissance)", 85, "Degat");
-Pokelol.Capacite Ahram_capacite3 = new("Kitsucharge = ", "Augmente la vitesse de 30% ", 1.3f, "Speed+");
-
-Pikaren.AddCapacite(Pikaren_capacite1);
-Pikaren.AddCapacite(Pikaren_capacite2);
-Pikaren.AddCapacite(Pikaren_capacite3);
-
-Ectoplari.AddCapacite(Ectoplari_capacite1);
-Ectoplari.AddCapacite(Ectoplari_capacite2);
-Ectoplari.AddCapacite(Ectoplari_capacite3);
-
-Swainquaza.AddCapacite(Swainquaza_capacite1);
-Swainquaza.AddCapacite(Swainquaza_capacite2);
-Swainquaza.AddCapacite(Swainquaza_capacite3);
-
-Ahram.AddCapacite(Ahram_capacite1);
-Ahram.AddCapacite(Ahram_capacite2);
-Ahram.AddCapacite(Ahram_capacite3);
-
-Joueur = Ectoplari;
-Ennemi = Swainquaza;
 
 float calcul_degat(Pokelol Attanquant, Pokelol Defenseur, int i)
 {
-    
-        Random random = new Random();
-        // Générer un nombre aléatoire entre 0 et 99 inclus
-        int randomValue = random.Next(0, 100);
-        float coup_critique = 1.0f;    
-        
-        if (randomValue <= 33)
-        {
-            coup_critique = 1.5f;
-        }
 
-    float pv_perdu = (((Attanquant.Attaque * Attanquant.Capacites[i].Stats_capacite)/(Defenseur.Defense * 50 + 2 )) * coup_critique);
+    Random random = new Random();
+    // Générer un nombre aléatoire entre 0 et 99 inclus
+    int randomValue = random.Next(0, 100);
+    float coup_critique = 1.0f;
+
+    if (randomValue <= 33)
+    {
+        coup_critique = 1.5f;
+    }
+
+    float pv_perdu = (((Attanquant.Stats.Attaque * Attanquant.Capacites[i].Stats_capacite) / (Defenseur.Stats.Defense * 50 + 2)) * coup_critique);
     return pv_perdu;
 }
 
@@ -75,28 +26,28 @@ void AppliquerEffetCapacite(Pokelol Attaquant, Pokelol Defenseur, int res)
     switch (Attaquant.Capacites[res].Type_capacite)
     {
         case "Degat":
-            Defenseur.Vie -= calcul_degat(Attaquant, Defenseur, res);
-            Console.WriteLine("Le defenseur perd "+ calcul_degat(Attaquant, Defenseur, res) + "point de vie");
+            Defenseur.Stats.Vie -= calcul_degat(Attaquant, Defenseur, res);
+            Console.WriteLine(Defenseur.Name+ " perd " + calcul_degat(Attaquant, Defenseur, res) + "point de vie");
             Console.WriteLine();
             break;
         case "Attaque-":
-            Defenseur.Attaque -= Attaquant.Capacites[res].Stats_capacite;
-            Console.WriteLine("Le defense perd " + Attaquant.Capacites[res].Stats_capacite + "de degat");
+            Defenseur.Stats.Attaque *= Attaquant.Capacites[res].Stats_capacite;
+            Console.WriteLine(Defenseur.Name + " perd " +  (100-100*Attaquant.Capacites[res].Stats_capacite) + "% d'attaque");
             Console.WriteLine();
             break;
         case "Speed+":
-            Attaquant.Speed *= Attaquant.Capacites[res].Stats_capacite;
-            Console.WriteLine("L'attaquant multiplie sa vitesse par " + Attaquant.Capacites[res].Stats_capacite);
+            Attaquant.Stats.Vitesse *= Attaquant.Capacites[res].Stats_capacite;
+            Console.WriteLine(Attaquant.Name + " augmente sa vitesse de " + (100*Attaquant.Capacites[res].Stats_capacite-100) + "%");
             Console.WriteLine();
             break;
         case "Speed-":
-            Defenseur.Speed *= Attaquant.Capacites[res].Stats_capacite;
-            Console.WriteLine("Le defenseur perd de la vitesse par " + Attaquant.Capacites[res].Stats_capacite);
+            Defenseur.Stats.Vitesse *= Attaquant.Capacites[res].Stats_capacite;
+            Console.WriteLine(Defenseur.Name + " perd " + (100-100*Attaquant.Capacites[res].Stats_capacite) + "% de vitesse");
             Console.WriteLine();
             break;
         case "Defense+":
-            Attaquant.Defense *= Attaquant.Capacites[res].Stats_capacite;
-            Console.WriteLine("L'attaquant multiplie sa defense par " + Attaquant.Capacites[res].Stats_capacite);
+            Attaquant.Stats.Defense *= Attaquant.Capacites[res].Stats_capacite;
+            Console.WriteLine(Attaquant.Name + " augmente sa defense de " + (100*Attaquant.Capacites[res].Stats_capacite-100) + "%");
             Console.WriteLine();
             break;
         default:
@@ -105,96 +56,175 @@ void AppliquerEffetCapacite(Pokelol Attaquant, Pokelol Defenseur, int res)
     }
 }
 
+
 void afficher_stats(Pokelol Joueur, Pokelol Ennemi)
 {
-    Console.WriteLine(Joueur.Name+ "Vie: " + Joueur.Vie+ " Attaque: " + Joueur.Attaque+ " Defense: " + Joueur.Defense+ " Speed: " + Joueur.Speed + "\n");
+    Console.WriteLine(Joueur.Name+ " Vie: " + Joueur.Stats.Vie + " || Attaque: " + Joueur.Stats.Attaque + " || Defense: " + Joueur.Stats.Defense + " || Speed: " + Joueur.Stats.Vitesse + "\n");
 
-    Console.WriteLine(Ennemi.Name+ "Vie: " + Ennemi.Vie + " Attaque: " + Ennemi.Attaque + " Defense: " + Ennemi.Defense + " Speed: " + Ennemi.Speed);
+    Console.WriteLine(Ennemi.Name+ " Vie: " + Ennemi.Stats.Vie + " || Attaque: " + Ennemi.Stats.Attaque + " || Defense: " + Ennemi.Stats.Defense + " || Speed: " + Ennemi.Stats.Vitesse + "\n");
 }
 
-while (Joueur.Vie >=0 & Ennemi.Vie >=0)
+
+void Combat(Pokelol joueur1, Pokelol joueur2)
 {
-
-    if (Joueur.Speed > Ennemi.Speed)
+    while (joueur1.Stats.Vie > 0 && joueur2.Stats.Vie > 0)
     {
-        Console.WriteLine();
-        Console.WriteLine("C'est à vous, que souhaitez-vous faire ?");
-        Console.WriteLine("1- Attaques\n2- Objets\n3- Fuite\n");
-        string choix = Console.ReadLine();
-        switch (choix)
-        {
-            case ("1"):
-                Console.WriteLine("Choisir parmi les compétences\n");
-                for (int i = 0; i < 3; i++)
-                {
-                    Console.WriteLine(Joueur.Capacites[i].Name_capacite +  Joueur.Capacites[i].Description);
-                }
-                
-                string value = Console.ReadLine();
-                int res = Convert.ToInt32(value) - 1;
-                Console.Clear();
-                Console.WriteLine("Tour du Joueur");
-                AppliquerEffetCapacite(Joueur, Ennemi, res);
-
-
-                Console.WriteLine("Tour de l'Ennemi");
-                Random random = new Random();
-                int randomability = random.Next(0,2);
-                AppliquerEffetCapacite(Ennemi, Joueur, res);
-                Console.WriteLine();
-
-                afficher_stats(Joueur, Ennemi);
-
-                break;
-            case "2":
-                break;
-            case "3":
-                break;
-        }
+        TourJoueur(joueur1, joueur2);
+        if (joueur2.Stats.Vie <= 0)
+            break;
+        TourEnnemi(joueur1, joueur2);
     }
-    
 }
 
-public class Pokelol
+void TourJoueur(Pokelol joueur1, Pokelol joueur2)
 {
-    public string Name { get; set; }
-    public float Vie { get; set; }
-    public float Attaque { get; set; }
-    public float Defense { get; set; }
-    public float Speed { get; set; }
-    public List<Capacite> Capacites { get; set; }
-
-    public Pokelol(string name, float vie, float attaque, float defense, float speed)
+    Console.WriteLine();
+    Console.WriteLine("C'est à vous, que souhaitez-vous faire ?");
+    Console.WriteLine("1- Attaques\n2- Objets\n3- Fuite\n");
+    string choix = Console.ReadLine();
+    switch (choix)
     {
-        Name = name;
-        Vie = vie;
-        Attaque = attaque;
-        Defense = defense;
-        Speed = speed;
-        Capacites = new List<Capacite> { };
+        case "1":
+            Console.WriteLine("Choisir parmi les compétences\n");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(joueur1.Capacites[i].Name_capacite + joueur1.Capacites[i].Description);
+            }
+
+            string value = Console.ReadLine();
+            int res = Convert.ToInt32(value) - 1;
+            Console.Clear();
+            Console.WriteLine("TOUR DU JOUEUR");
+            AppliquerEffetCapacite(joueur1, joueur2, res);
+            afficher_stats(joueur1, joueur2);
+            break;
+        case "2":
+            // Gérer l'utilisation d'objets
+            break;
+        case "3":
+            // Gérer la fuite
+            break;
     }
+}
 
-    public void AddCapacite(Capacite capacite)
+void TourEnnemi(Pokelol joueur1, Pokelol joueur2)
+{
+    Console.WriteLine("TOUR DE L'ENNEMI");
+    Random random = new Random();
+    int randomAbility = random.Next(0, 2);
+    AppliquerEffetCapacite(joueur2, joueur1, randomAbility);
+    afficher_stats(joueur1, joueur2);
+}
+
+class Programme
+{
+
+
+
+    static void Main()
     {
-        Capacites.Add(capacite);
-    }
+        //CREATION D'UN NOUVEAU JOUEUR
+        Player player = new Player("Player");
 
-    
+        //DEFINITION DES POKELOL ET DE LEURS STATISTIQUES
+        Pokelol Pikaren = new("Pikaren");
+        Pikaren.DefinirStatistiques(233, 183, 147, 151);
+        Pokelol Ectoplari = new("Ectoplari");
+        Ectoplari.DefinirStatistiques(213, 162, 142, 182);
+        Pokelol Swainquaza = new("Swainquaza");
+        Swainquaza.DefinirStatistiques(212, 202, 142, 147);
+        Pokelol Ahram = new("Ahram");
+        Ahram.DefinirStatistiques(207, 137, 152, 187);
 
-    public class Capacite
-    {
-        public string Name_capacite { get; set; }
-        public string Description { get; set; }
+        //CAPACITE DE PIKAREN
+        Pokelol.Capacite Pikaren_capacite1 = new("Elekdemacia = ", " Inflige des degats (80 puissance)", 80, "Degat");
+        Pokelol.Capacite Pikaren_capacite2 = new("Pikarush = ", "Augmente la vitesse de 15%", 1.15f, "Speed+");
+        Pokelol.Capacite Pikaren_capacite3 = new("Gabri = ", "Augmente la defense de 30%", 1.3f, "Defense+");
 
-        public float Stats_capacite { get; set; }
-        public string Type_capacite { get; set; }
+        //CAPACITE DE ECTOPLARI
+        Pokelol.Capacite Ectoplari_capacite1 = new("LaserOmbre = ", "Inflige des degats(110 puissance)", 110, "Degat");
+        Pokelol.Capacite Ectoplari_capacite2 = new("Exploplasma = ", "Inflige des degats(90 puissance)", 90, "Degat");
+        Pokelol.Capacite Ectoplari_capacite3 = new("Taclebas = ", "Baisse la vitesse de l'ennemi de 20% ", 0.8f, "Speed-");
 
-        public Capacite(string name_capacite, string description,float stats_capacite, string type_capacite)
+        //CAPACITE DE SWAINQUAZA
+        Pokelol.Capacite Swainquaza_capacite1 = new("CorbeauMeteore = ", "Inflige des degats(150 puissance)", 150, "Degat");
+        Pokelol.Capacite Swainquaza_capacite2 = new("GriffeAscension = ", "Inflige des degats(110 puissance)", 110, "Degat");
+        Pokelol.Capacite Swainquaza_capacite3 = new("OeilViolent = ", "Inflige des degats(90 puissance)", 90, "Degat");
+
+        //CAPACITE DE AHRAM
+        Pokelol.Capacite Ahram_capacite1 = new("FlammeCharme = ", " Baisse l'attaque de l'adversaire de 30% ", 0.85f, "Attaque-");
+        Pokelol.Capacite Ahram_capacite2 = new("Dracoboule = ", "Inflige des degats(85 puissance)", 85, "Degat");
+        Pokelol.Capacite Ahram_capacite3 = new("Kitsucharge = ", "Augmente la vitesse de 30% ", 1.3f, "Speed+");
+
+        Pikaren.AddCapacite(Pikaren_capacite1);
+        Pikaren.AddCapacite(Pikaren_capacite2);
+        Pikaren.AddCapacite(Pikaren_capacite3);
+
+        Ectoplari.AddCapacite(Ectoplari_capacite1);
+        Ectoplari.AddCapacite(Ectoplari_capacite2);
+        Ectoplari.AddCapacite(Ectoplari_capacite3);
+
+        Swainquaza.AddCapacite(Swainquaza_capacite1);
+        Swainquaza.AddCapacite(Swainquaza_capacite2);
+        Swainquaza.AddCapacite(Swainquaza_capacite3);
+
+        Ahram.AddCapacite(Ahram_capacite1);
+        Ahram.AddCapacite(Ahram_capacite2);
+        Ahram.AddCapacite(Ahram_capacite3);
+
+
+        player.Team.AddMember(Pikaren);
+        player.Team.AddMember(Swainquaza);
+
+        Item potion1 = new Item { Name = "Healing Potion" };
+        Item potion2 = new Item { Name = "Mana Potion" };
+
+        player.Inventory.AddItem(potion1);
+        player.Inventory.AddItem(potion2);
+        player.Team.Members[0];
+
+        while (true)
         {
-            Name_capacite = name_capacite;
-            Description = description;
-            Stats_capacite = stats_capacite;
-            Type_capacite = type_capacite;
+            Console.WriteLine("Menu:");
+            Console.WriteLine("E. Team");
+            Console.WriteLine("I. Inventory");
+            Console.WriteLine("W. Combat");
+            Console.WriteLine("Q. Quit");
+
+            Console.Write("Choose an option: ");
+            string choice = Console.ReadLine();
+
+            switch (choice)
+            {
+                case "e":
+                    Console.Clear();
+                    player.Team.ShowTeam();
+                    break;
+
+                case "i":
+                    Console.Clear();
+                    player.Inventory.ShowInventory();
+                    break;
+
+                case "q":
+                    Console.WriteLine("Goodbye!");
+                    return;
+
+                case "w":
+                    player.Team.ShowTeam();
+                    Console.WriteLine("choisi un pokemon pour combattre");
+                    string choix_equipe = Console.ReadLine();
+                    int result = Convert.ToInt32(choix_equipe) - 1;
+                    Combat(player.Team.Members[result], Ahram);
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid option. Please choose again.");
+                    break;
+
+            }
+
+            Console.ReadLine();
         }
     }
 }
